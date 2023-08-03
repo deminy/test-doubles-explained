da85a5e6df1024d57ce4cf2ea45aa95f81c716d6
Demin Yin <deminy@deminy.net>
2023-08-03 16:18:51 -0700

    documentation updates

--- TestClass.php
+++ TestClass.php
@@ -4,24 +4,31 @@ declare(strict_types=1);
 
 class TestClass
 {
+    /**
+     * A public integer property with a default value of 1.
+     *
+     * The value of this property could be changed in two ways:
+     * 1. pass in a different integer value when creating an object of this class.
+     * 2. by calling method $this->setVar().
+     */
     public int $var = 1;
 
     public function __construct(int $var = 2)
     {
-        $this->debug("TestClass::__construct() is invoked with parameter {$var}.");
+        $this->debug("TestClass::__construct() is executed with an integer argument {$var}.");
         $this->var = $var;
     }
 
     public function setVar(int $var): self
     {
-        $this->debug("TestClass::setVar() is invoked with parameter {$var}.");
+        $this->debug("TestClass::setVar() is executed with an integer argument {$var}.");
         $this->var = $var;
         return $this;
     }
 
     public function getVar(): int
     {
-        $this->debug("TestClass::getVar() is invoked with return value {$this->var}.");
+        $this->debug("TestClass::getVar() is executed with an integer return value {$this->var}.");
         return $this->var;
     }
 
